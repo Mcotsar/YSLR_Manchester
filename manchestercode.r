a68aac83a155c209673678b3ce3fc1402e581e06
Maria <mcotsar@gmail.com>
2017-01-24 16:58:53 +0100

    merge some modif on the model and intro

--- manchestercode.r
+++ manchestercode.r
@@ -66,7 +66,7 @@ library(gridExtra)
 spatialDist <- data.frame(site=c('parlamento','belen','malpica','delicias', 'villaseca'), distance=c(0,68,74,82,95))
 spatialDist$site <- factor(spatialDist$site, levels = c('parlamento','belen','malpica', 'delicias','villaseca'))
 
-g1 <- ggplot(spatialDist, aes(x=distance, y=0, col=site)) + xlim(c(0, 100)) + geom_segment(col='grey40', x=0, xend=max(spatialDist$distance), y=0, yend=0) + geom_point()  + geom_text(aes(label=site), vjust=-1)  + theme_bw() + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major=element_blank(), panel.grid.minor=element_blank(), axis.title.y=element_blank()) + xlab('distance (km)') + scale_color_manual(values=c("skyblue3","goldenrod1","indianred2","palegreen4", "darkorange3")) + theme(legend.position='none') + ylim(c(-0.05,0.1))
+g1 <- ggplot(spatialDist, aes(x=distance, y=0, col=site)) + xlim(c(0, 100)) + geom_segment(col='grey40', x=0, xend=max(spatialDist$distance), y=0, yend=0) + geom_point()  + geom_text(aes(label=site), vjust=-1,size=7)  + theme_bw() + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major=element_blank(), panel.grid.minor=element_blank(), axis.title.y=element_blank()) + xlab('distance (km)') + scale_color_manual(values=c("skyblue3","goldenrod1","indianred2","palegreen4", "darkorange3")) + theme(legend.position='none') + ylim(c(-0.05,0.1))
 
 
 # PCA data
@@ -85,12 +85,12 @@ pcaLogValues$site <- factor(bar$site, levels = c('parlamento','belen','malpica',
 
 #with geom_density2d (alpha means the density 0 less density 10 more density (to see the lines more)
 
-g2 <- ggplot(pcaLogValues, aes(y=Comp.1, x=Comp.2, col=site)) + geom_density2d(aes(col=site), alpha=0.2) + geom_point() + facet_wrap(~site, ncol=5) + theme_bw() + theme(legend.position='none') + scale_color_manual(values=c("skyblue3","goldenrod1","indianred2","palegreen4", "darkorange3"))
+g2 <- ggplot(pcaLogValues, aes(y=Comp.1, x=Comp.2, col=site)) + geom_density2d(aes(col=site), alpha=0.3) + geom_point(aes(size = 3)) + facet_wrap(~site, ncol=5) + theme_bw() + theme(legend.position='none') + scale_color_manual(values=c("skyblue3","goldenrod1","indianred2","palegreen4", "darkorange3"))
 
 
 # final composition
 
-svg('pcs.svg', width=25, height=5)    
+pdf('fig1.pdf', width=20, height=5)    
 grid.arrange(arrangeGrob(g2,g1,heights=c(2/3, 1/3)))
 dev.off()
 
